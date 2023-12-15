79ccba9204b7ac1d7921d30cf6bb15be3c14e96f
dangodman <2488399989@qq.com>
2023-12-15 22:10:55 +0800

    first

---
+++ src/views/Root.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="h-screen w-screen">
+    <Header />
+    <el-row class="mb-4">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+    <el-button type="danger">Danger</el-button>
+  </el-row>
+    <!-- 底部导航 -->
+    <div class="bg-green-700 text-3xl text-center text-white
+     fixed inset-x-0 bottom-0 p-4">
+      <Footer></Footer>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Footer from '@/components/layout/footer/Footer.vue'
+import Header from '@/components/layout/header/Header.vue'
+</script>
+
+<style  scoped></style>
